b96b3c1ab99d9d3ffa1ce1c79ad00a7447f5b008
autoTest <autotest@buaa>
2017-03-12 00:45:10 +0800

    Initial lab2

--- include.mk
+++ include.mk
@@ -6,5 +6,5 @@
 
 CROSS_COMPILE := /OSLAB/compiler/usr/bin/mips_4KC-
 CC			  := $(CROSS_COMPILE)gcc
-CFLAGS		  := -O -G 0 -mno-abicalls -fno-builtin -Wa,-xgot -Wall -fPIC
+CFLAGS		  := -O -G 0 -mno-abicalls -fno-builtin -Wa,-xgot -Wall -fPIC 
 LD			  := $(CROSS_COMPILE)ld
