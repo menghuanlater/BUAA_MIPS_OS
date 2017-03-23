0878f7862b33ad3b7e5f81b4b74bff1c5ae3bfce
menghuanlater <anlin781205936@126.com>
2017-03-23 15:14:00 +0800

    15061134

--- mm/pmap.c
+++ mm/pmap.c
@@ -6,7 +6,7 @@
 
 
 
-/* These variables are set by mips_detect_memory() */
+/*xs These variables are set by mips_detect_memory() */
 u_long maxpa;            /* Maximum physical address */
 u_long npage;            /* Amount of memory(in pages) */
 u_long basemem;          /* Amount of base memory(in bytes) */
