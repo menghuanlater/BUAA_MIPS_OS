48c1fd947543c5331657a1b01003cf394c2b29e3
menghuanlater <anlin781205936@126.com>
2017-03-27 13:18:00 +0800

    15061134_end

--- lib/print.c
+++ lib/print.c
@@ -6,7 +6,7 @@
  * under  the terms of  the GNU General  Public License as published by the
  * Free Software Foundation;  either version 2 of the  License, or (at your
  * option) any later version.
- *
+ * delecc
  */
 
 #include	<print.h>
