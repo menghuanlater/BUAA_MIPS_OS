cdf6e82542c8debe8d231cc6837b0c1212ca1a55
menghuanlater <anlin781205936@126.com>
2017-03-30 15:45:03 +0800

    15061134

--- lib/sched.c
+++ lib/sched.c
@@ -11,29 +11,19 @@
  * Hints:
  *  The variable which is for counting should be defined as 'static'.
  */
- 
+//when occur next exception break,carry out this function again. 
 void sched_yield(void)
 {
-	static long position = 0;
-	for(;position<NENV;position++){
+	static long position = -1;
+	int i;
+	for(i=0;i<NENV;i++){
+		position++;//we must carry this at head,because put it back will cause all env use same position
+		if(position==NENV){ //when it over limit range,then we reset it to the array head.
+			position = 0;
+		}
 		if(envs[position].env_status == ENV_RUNNABLE){
 			env_run(&envs[position]);
-			if(position==NENV-1)
-				position = 0;
-			else
-				position++;
 			return;
 		}
 	}
 }
-/*void sched_yield(void){
-	static long i = -1;
-	int n = 0;
-	for(;n<NENV;n++){
-		i++;
-		if(i==NENV)
-			i=0;
-		if(envs[i].env_status == ENV_RUNNABLE)
-			env_run(&envs[i]);
-	}
-}*/
