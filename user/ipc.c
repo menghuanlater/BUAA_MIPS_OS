c80a045105fa2a5a60acaedeb97450a36d11ec98
menghuanlater <anlin781205936@126.com>
2017-05-16 13:43:46 +0800

    15061134

--- user/ipc.c
+++ user/ipc.c
@@ -44,4 +44,23 @@ ipc_recv(u_int *whom, u_int dstva, u_int *perm)
 
 	return env->env_ipc_value;
 }
+void new_ipc_send(u_int whom,u_int val,u_int srcva,u_int perm){
+	int r;
+	while((r=syscall_ipc_can_send(whom,val,srcva,perm))==-E_IPC_NOT_RECV){
+		syscall_yield();
+	}
+	if(r==0){
+		return;
+	}
+}
+u_int new_ipc_recv(u_int *whom,u_int dstva,u_int *perm){
+	syscall_ipc_recv(dstva);
+	if(whom){
+		*whom = env->env_ipc_from;
+	}
+	if(perm){
+		*perm = env->env_ipc_perm;
+	}
+	return env->env_ipc_value;
+}
 
