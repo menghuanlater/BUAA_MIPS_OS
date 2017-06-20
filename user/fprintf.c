a9e76bd2a529dc4ca0d0fdc45c8f48d3e0541cdc
menghuanlater <anlin781205936@126.com>
2017-06-20 17:40:17 +0800

    15061134

--- user/fprintf.c
+++ user/fprintf.c
@@ -17,12 +17,12 @@ static void user_out2string(void *arg, char *s, int l)
 
 int fwritef(int fd, const char *fmt, ...)
 {
-	char buf[512];
-	user_bzero(buf,sizeof(buf));
+	char buf[512]={'\0'};
+	//user_bzero((void *)buf,512);
 	va_list ap;
 	va_start(ap, fmt);
 	user_lp_Print(user_out2string, buf, fmt, ap);
 	//writef("");
 	va_end(ap);
-	return write(fd, buf, strlen(buf));
+	return write(fd, buf, strlen(buf)+1);
 }
